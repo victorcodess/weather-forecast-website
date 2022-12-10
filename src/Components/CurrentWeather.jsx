23c89aabd1395ba2ee6e187ba9e64c9224c6c6b0
Victor Williams <victor.williams@stu.cu.edu.ng>
2022-12-10 14:17:06 +0100

    created current comp

--- src/Components/CurrentWeather.jsx
+++ src/Components/CurrentWeather.jsx
@@ -1,13 +1,22 @@
-const CurrentWeather = () => {
-    return (
-        <div>
-            <h1 className="current-temp">31°C</h1>
-            <div className="location-date">
-                <h2 className="location">London</h2>
-                <h3 className="date">06:09 - Monday, 9 September 2022</h3>
-            </div>
-        </div>
-    )
-}
+import "./CurrentWeather.css";
+import { WiDayCloudy } from "react-icons/wi";
 
-export default CurrentWeather
+const CurrentWeather = () => {
+  return (
+    <div className="current">
+      <h1 className="current-temp">
+        31<sup>°C</sup>
+      </h1>
+      <div className="location-date">
+        <h2 className="location">London</h2>
+        <h3 className="current-date">06:09 - Monday, 9 September 2022</h3>
+      </div>
+      <div className="current-weather">
+        <WiDayCloudy className="current-weather__icon" />
+        {/* <h3 className="current-weather__text">Mostly Sunny</h3> */}
+      </div>
+    </div>
+  );
+};
+
+export default CurrentWeather;
