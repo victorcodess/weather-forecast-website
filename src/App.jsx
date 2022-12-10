e983581e9ef67197f7532dbbe41122beb07fec51
Victor Williams <victor.williams@stu.cu.edu.ng>
2022-12-10 11:57:14 +0100

    added searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import SearchBar from "./Components/SearchBar";
+import CurrentWeather from "./Components/CurrentWeather";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [weather, setWeather] = useState([]);
+
+  // useEffect(() => {
+  //   fetch(
+  //     "http://api.weatherapi.com/v1/current.json?key=04c60fe1f2624fff885223022220912&q=London&aqi=no"
+  //   )
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       // console.log(data);
+  //       setWeather(data);
+  //     })
+  //     .catch((err) => {
+  //       // console.log(err.message);
+  //     });
+  // }, []);
+
+  // console.log(weather);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SearchBar />
+
+      <CurrentWeather/>
+      {/* {JSON.stringify(weather.current.temp_f)} */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/CurrentWeather.jsx
@@ -0,0 +1,13 @@
+const CurrentWeather = () => {
+    return (
+        <div>
+            <h1 className="current-temp">31°C</h1>
+            <div className="location-date">
+                <h2 className="location">London</h2>
+                <h3 className="date">06:09 - Monday, 9 September 2022</h3>
+            </div>
+        </div>
+    )
+}
+
+export default CurrentWeather
